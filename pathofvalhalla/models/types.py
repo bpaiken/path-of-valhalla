32a6c7baae4f1ce193f21f61b1d6783bf0ab13d3
Brian Aiken <baiken@capaxglobal.com>
2020-11-24 14:03:59 -0500

    add initial skill tree data input json

--- pathofvalhalla/models/types.py
+++ pathofvalhalla/models/types.py
@@ -10,3 +10,22 @@ class SkillNodeType:
     @classmethod
     def notable(cls):
         return 'notable'
+    
+    @classmethod
+    def origin(cls):
+        return 'origin'
+
+class StatType:
+    @classmethod
+    def health(cls):
+        return 'health'
+    @classmethod
+    def armor(cls):
+        return 'armor'
+    @classmethod
+    def weight(cls):
+        return 'weight'
+    @classmethod
+    def evasion(cls):
+        return 'evasion'
+    
